1b3dee06457be6fc8c931f4c6b8e678640ecdc60
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-07-15 17:54:08 -0500

    feat: :sparkles: DELETE EQUIPOS FUNCIONAL

--- backend/controllers/equipos.controller.js
+++ backend/controllers/equipos.controller.js
@@ -2,7 +2,7 @@ import Equipos from "../model/Equipos.js";
 
 const getEquipos = async(req, res)=>{
     try {
-        const equipos = await Equipos.findOne();
+        const equipos = await Equipos.find();
         res.json(equipos);
     } catch (error) {
         res.status(500);
@@ -20,7 +20,20 @@ const getOneEquipos = async(req, res)=>{
     }
 }
 
+const borrarEquipos = async(req, res)=>{
+    try {
+        await Equipos.deleteOne({_id: req.params.id});
+        res.json({
+            "message": "Eliminado satisfactoriamente"
+        })
+    } catch (error) {
+        res.status(500);
+        res.json(error.message);
+    }
+}
+
 export {
     getEquipos,
-    getOneEquipos
+    getOneEquipos,
+    borrarEquipos
 }
--- backend/routes/equipos.routes.js
+++ backend/routes/equipos.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getEquipos, getOneEquipos } from "../controllers/equipos.controller.js";
+import { getEquipos, getOneEquipos, borrarEquipos } from "../controllers/equipos.controller.js";
 
 
 const router = Router();
 
 router.get('/', getEquipos);
 router.get('/:id', getOneEquipos);
+router.delete('/delete/:id', borrarEquipos);
 
 export default router;
